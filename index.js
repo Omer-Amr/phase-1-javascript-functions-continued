708e8a377a18266cdab860acfa79fceda540b2bb
omer <you@example.com>
2022-11-21 21:16:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun (activity) {
+    if (activity) {
+        return (`This Saturday, I want to ${activity}!`);  
+    }else{
+        return (`This Saturday, I want to roller-skate!`);
+    } 
+}
+
+let mondayWork = function (activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="a hard worker") {
+      return `You are ${style}${adjective}${style}!`
+    }
+}
+
